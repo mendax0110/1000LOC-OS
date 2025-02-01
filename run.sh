4cffb9181c7500f8e464f4b6f6cce8d70fbb2d6d
mendax0110 <adrian.goessl@outlook.com>
2025-02-02 00:52:01 +0100

    [1000LOC-OS][fixes]adapted the shell script, fixed issues in the linked scripts
    Signed-off-by: mendax0110 <adrian.goessl@outlook.com>

--- run.sh
+++ run.sh
@@ -7,22 +7,35 @@ QEMU=qemu-system-riscv32
 # Path to clang and compiler flags
 CC=/opt/homebrew/opt/llvm/bin/clang
 OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
-CFLAGS="--std=c11 -O2 -g3 -Wall -Wextra --target-riscv32 -ffreestanding -nostdlib"
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# absolute path to project root
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+BUILD_DIR="$SCRIPT_DIR/build"
+
+# check we are in the build directory
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
 
 # Build the shell
-$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$CC $CFLAGS -Wl,-T"$SCRIPT_DIR/user.ld" -Wl,-Map=shell.map -o shell.elf \
+    "$SCRIPT_DIR/shell.c" "$SCRIPT_DIR/user.c" "$SCRIPT_DIR/common.c"
+
 $OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
-$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+$OBJCOPY -I binary -O elf32-littleriscv shell.bin shell.bin.o
 
 # Build the kernel
-$CC $CFLAGS -W1,-Tkernel.ld -W1,-MAP=kernel.map -o kernel.elf \
-    kernel.c common.c shell.bin.o
+$CC $CFLAGS -Wl,-T"$SCRIPT_DIR/kernel.ld" -Wl,-Map=kernel.map -o kernel.elf \
+    "$SCRIPT_DIR/kernel.c" "$SCRIPT_DIR/common.c" shell.bin.o
 
-(cd disk && tar cf ../disk.tar --format=ustar *.txt)
+# Create disk image
+mkdir -p disk
+cp -r "$SCRIPT_DIR/disk/"*.txt disk/
+tar cf disk.tar --format=ustar -C disk .
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot  \
-    -d unimp,guest_errors,int,cpu_reset -D qemu.log                         \
-    -drive id=drive0,file=disk.tar,format=raw,if=none                       \
-    -drive virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0             \
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot    \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log                           \
+    -drive id=drive0,file=disk.tar,format=raw,if=none                         \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0              \
     -kernel kernel.elf
--- user.h
+++ user.h
@@ -13,4 +13,4 @@ void putchar(char ch);
 int getchar(void);
 int readfile(const char *filename, char *buf, int len);
 int writefile(const char *filename, const char *buf, int len);
-__attribute__((noreturn)) void exit(int code);
+__attribute__((noreturn)) void exit(void);
